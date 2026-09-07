41381aaf4e25ad0d4fcaae6b867727b23c5d936d
AaronL <Aaronlaurio0@gmail.com>
2026-09-06 19:47:18 -0700

    First Warrior

---
+++ BattleArena/BattleArena/Program.cs
@@ -0,0 +1,38 @@
+﻿using BattleArena.Warriors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleArena
+{
+    
+    internal class Progress
+
+    {
+        static void Main(string[] args)
+
+        {
+            Warrior Den = new Warriors.Warrior();
+            Den.Name = "Den";
+            Den.Health = 100;
+            Den.AttackPower = 30;
+
+            Warrior Lhi = new Warriors.Warrior();
+            Lhi.Name = "Lhi";
+            Lhi.Health = 500;
+            Lhi.AttackPower = 5;
+
+            Console.WriteLine($"{Den.Name} has " +
+    $"{Den.Health} health and {Den.AttackPower} attack power.");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine($"{Lhi.Name} has " +
+                $"{Lhi.Health} health and {Lhi.AttackPower} attack power.");
+            Console.WriteLine("----------------------------------------------");
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ BattleArena/BattleArena/Warriors/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleArena.Warriors
+{
+    class Warrior
+    {
+        public string Name;
+
+        public int Health;
+
+        public int AttackPower;
+
+
+
+    }
+}
