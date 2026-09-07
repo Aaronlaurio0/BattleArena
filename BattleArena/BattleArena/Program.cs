fa59716081cafe32373b3cdba104a99e92ab6510
AaronL <Aaronlaurio0@gmail.com>
2026-09-06 23:18:01 -0700

    Add battle loop and special attack to warrior class

--- BattleArena/BattleArena/Program.cs
+++ BattleArena/BattleArena/Program.cs
@@ -7,26 +7,29 @@ using System.Threading.Tasks;
 
 namespace BattleArena
 {
-    
+
     internal class Progress
 
     {
         static void Main(string[] args)
-
         {
-            Warrior Den = new Warriors.Warrior("Den", 100, 30);
-            Warrior Lhi = new Warriors.Warrior("Lhi", 500, 5);
+            int round = 1;
+            Warrior Den = new Warriors.Warrior("Den", 100, 30, "Death Mark");
+            Warrior Lhi = new Warriors.Warrior("Lhi", 500, 5, "Shouting Wave");
 
+            Den.DisplayStatus();
+            Lhi.DisplayStatus();
 
-            Console.WriteLine($"{Den.Name} has " +
-    $"{Den.Health} health and {Den.AttackPower} attack power.");
-            Console.WriteLine("----------------------------------------------");
-            Console.WriteLine($"{Lhi.Name} has " +
-                $"{Lhi.Health} health and {Lhi.AttackPower} attack power.");
-            Console.WriteLine("----------------------------------------------");
-            Console.ReadLine();
+            while (Den.IsAlive && Lhi.IsAlive)
+            {
+                Console.WriteLine($"--------Round  {round}----------");
+                Den.Attack(Lhi);
+                Lhi.Attack(Den);
+                Console.WriteLine("---------------------------------");
+                round++;
+            }
 
+            Console.ReadKey();
         }
-
     }
 }
---
+++ BattleArena/BattleArena/Warriors/Fighter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleArena.Warriors
+{
+    public class Fighter : Warrior
+    {
+        public Fighter(string name, int health, int attackPower, string specialAttackName) 
+            : base(name, health, attackPower, specialAttackName)
+        {
+
+        }
+
+
+    }
+}
